ebe470ddad809ea621cb891f136216e5a90f30b5
Daniel Esp <dany_aec@hotmail.com>
2026-03-29 13:56:32 -0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,77 @@
+window.menuData = {
+  "Desayunos": [
+    ["Huevos Rancheros",55],
+    ["Huevos a la Mexicana",55],
+    ["Huevos con Jamón",55],
+    ["Huevos con Chorizo",55],
+    ["Huevos con Tocino",60],
+    ["Omelet de Jamón con Queso",85],
+    ["Omelet de Champiñones",85],
+    ["Tacos de pollo con guacamole",85],
+    ["Enchiladas",85],
+    ["Chilaquiles con pollo o huevo",85],
+    ["Chilaquiles con carne asada",180],
+    ["Hot Cakes",70],
+    ["Jugo de naranja",40],
+    ["Café",20],
+    ["Café con leche",25],
+    ["Cappuccino",55],
+    ["Chocolate",30]
+  ],
+
+  "Quesos y Ensaladas": [
+    ["Quesitos de la casa",16],
+    ["Choriqueso",110],
+    ["Queso fundido",110],
+    ["Ensalada de atún",95],
+    ["Ensalada de pollo",100],
+    ["Ensalada de camarón",120]
+  ],
+
+  "Sopas": [
+    ["Spaghetti",55],
+    ["Arroz con huevo",45],
+    ["Consomé de pollo",45],
+    ["Caldo Tlalpeño",45],
+    ["Sopa Azteca",60],
+    ["Sopa de ajo",60],
+    ["Sopa de cebolla",60],
+    ["Sopa de médula",75],
+    ["Crema de champiñones",65],
+    ["Crema de elote",65],
+    ["Caldo de camarón",115]
+  ],
+
+  "Carnes": [
+    ["T-Bone",245],
+    ["Arrachera",240],
+    ["Carne asada",180],
+    ["Carne a la tampiqueña",195],
+    ["Filete mignon",210],
+    ["Filete gratinado",225],
+    ["Milanesa",195],
+    ["Brocheta de filete",195],
+    ["Puntas a la mexicana",195]
+  ],
+
+  "Mariscos": [
+    ["Cóctel de camarón",100],
+    ["Cóctel campechana",100],
+    ["Ceviche de pescado",140],
+    ["Vuelve a la vida",140],
+    ["Camarones al mojo de ajo",210],
+    ["Camarones al ajillo",210],
+    ["Camarones a la diabla",210],
+    ["Camarones empanizados",210],
+    ["Tacos de camarón",135]
+  ],
+
+  "Bebidas": [
+    ["Refresco",35],
+    ["Limonada",40],
+    ["Naranjada",40],
+    ["Jarra de agua",90],
+    ["Cerveza Corona",40],
+    ["Michelada",50]
+  ]
+};
